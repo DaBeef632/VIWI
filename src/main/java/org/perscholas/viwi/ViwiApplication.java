980b65ace5d336c64e24417c8a3f90665270deac
Greg Conti <53584920+DaBeef632@users.noreply.github.com>
2021-06-15 09:41:44 -0400

    first commit

---
+++ src/main/java/org/perscholas/viwi/ViwiApplication.java
@@ -0,0 +1,13 @@
+package org.perscholas.viwi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViwiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViwiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/perscholas/viwi/ViwiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.perscholas.viwi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViwiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
